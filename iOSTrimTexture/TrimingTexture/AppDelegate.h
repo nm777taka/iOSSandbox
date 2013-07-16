0172541c33a38460aa12e4cd93247bd12448ab69
ozawaseijiro <ozawa4751@gmail.com>
2013-07-16 12:57:53 +0900

    add trim texture

---
+++ iOSTrimTexture/TrimingTexture/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TrimingTexture
+//
+//  Created by Seijiro on 2013/07/08.
+//  Copyright (c) 2013 Seijiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSTrimTexture/TrimingTextureTests/TrimingTextureTests.m
@@ -0,0 +1,32 @@
+//
+//  TrimingTextureTests.m
+//  TrimingTextureTests
+//
+//  Created by Seijiro on 2013/07/08.
+//  Copyright (c) 2013 Seijiro. All rights reserved.
+//
+
+#import "TrimingTextureTests.h"
+
+@implementation TrimingTextureTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TrimingTextureTests");
+}
+
+@end
