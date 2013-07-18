1548ecc9be6b1bf49ba29a702960f986cb6cbb32
ozawaseijiro <ozawa4751@gmail.com>
2013-07-18 17:32:22 +0900

    gradationのInitialCommit

---
+++ gradation/gradation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  gradation
+//
+//  Created by Seijiro on 2013/07/18.
+//  Copyright (c) 2013 Seijiro. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ gradation/gradationTests/gradationTests.h
@@ -0,0 +1,13 @@
+//
+//  gradationTests.h
+//  gradationTests
+//
+//  Created by Seijiro on 2013/07/18.
+//  Copyright (c) 2013 Seijiro. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface gradationTests : SenTestCase
+
+@end
